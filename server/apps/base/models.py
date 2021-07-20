c8251a4bef8f7afac5765eba83ffa05ee3b6891b
Bambooh <cdgaviria40@misena.edu.co>
2021-07-20 09:31:21 -0500

    :sparkles: Base class, initial events class

---
+++ server/apps/base/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class BaseModel(models.Model):
+
+    id = models.autofield(primary_key=True)
+    state = models.BooleanField('Estado', default=True)
+    created_date = models.DateField('Fecha de Creacion', auto_now=False, auto_now_add=True)
+    modified_date = models.DateField('Fecha de Modificacion', auto_now=True, auto_now_add=False)
+    deleted_date = models.DateField('Fecha de Eliminacion', auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+        verbose_name= 'Modelo Base'
+        verbose_plural_name = 'Modelos Base'
