e289595e7e588fd7f0bf2143f8b518195a1ba001
Bambooh <cdgaviria40@misena.edu.co>
2021-07-21 13:26:14 -0500

    :recycle: Refactoring EventSerializers

--- server/apps/events/api/serializers/general_serializers.py
+++ server/apps/events/api/serializers/general_serializers.py
@@ -11,34 +11,6 @@ class EventSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class EventFormatedSerializer(serializers.ModelSerializer):
-    """
-    date = serializers.StringRelatedField()
-    place = serializers.StringRelatedField()
-    los anteriores causan probemas con las Views
-
-    members= MemberSerializer(many=True)
-    members = serializers.StringRelatedField(many=True)
-    """
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        print(data)
-        return{
-            'id': instance.id,
-            'name': instance.name,
-            'category': instance.category.name,
-            'date': instance.date,
-            'place': instance.place.name if instance.place != '' else '',
-            'members': {instance.members.name}
-        }
-
-    class Meta:
-        model = Event
-        exclude = ['state', 'created_date',
-                   'modified_date', 'deleted_date']
-
-
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
